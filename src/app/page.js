ab5436bc0aa55375229e186ecce76574e1aa494e
king-mayor <99553727+king-mayor@users.noreply.github.com>
2024-12-03 10:19:49 -0800

    optimized

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,23 @@
 "use client"
-import About from '@/components/About'
-import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
-import Hero from '@/components/Hero'
-import Navbar from '@/components/Navbar'
-import Projects from '@/components/Projects'
-import ScrollToTop from '@/components/ScrollToTop'
-import Testimonial from '@/components/Testimonial'
+// import About from '@/components/About'
+// import Contact from '@/components/Contact'
+// import Footer from '@/components/Footer'
+// import Hero from '@/components/Hero'
+// import Navbar from '@/components/Navbar'
+// import Projects from '@/components/Projects'
+// import ScrollToTop from '@/components/ScrollToTop'
+// import Testimonial from '@/components/Testimonial'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import React,{useEffect} from 'react'
-
+import React,{useEffect, lazy} from 'react'
+    const Hero = lazy(() => import('@/components/Hero'));
+    const Contact = lazy(() => import('@/components/Contact'));
+    const Footer = lazy(() => import('@/components/Footer'));
+    const Navbar = lazy(() => import('@/components/Navbar'));
+    const Projects = lazy(() => import('@/components/Projects'));
+    const ScrollToTop = lazy(() => import('@/components/ScrollToTop'));
+    const Testimonial = lazy(() => import('@/components/Testimonial'));
+    const About = lazy(() => import('@/components/About'));
 
 const Home = () => {
   useEffect(()=>{
